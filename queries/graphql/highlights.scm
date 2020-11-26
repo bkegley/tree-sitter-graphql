2f28bc9c5f99e99e7079f75d0a197ff0bcc6e0d4
Bryan Kegley <hi@bryankegley.me>
2020-11-26 13:09:40 -0600

    Add highlights

--- queries/graphql/highlights.scm
+++ queries/graphql/highlights.scm
@@ -1,43 +1,132 @@
+; Types
+;------
+
+(scalar_type_definition
+  (name) @type)
+
+(object_type_definition
+  (name) @type)
+
+(interface_type_definition
+  (name) @type)
+
+(union_type_definition
+  (name) @type)
+
+(enum_type_definition
+  (name) @type)
+
+(input_object_type_definition
+  (name) @type)
+
+(directive_definition
+  (name) @type)
+
+(scalar_type_extension
+  (name) @type)
+
+(object_type_extension
+  (name) @type)
+
+(interface_type_extension
+  (name) @type)
+
+(union_type_extension
+  (name) @type)
+
+(enum_type_extension
+  (name) @type)
+
+(input_object_type_extension
+  (name) @type)
+
+(named_type
+  (name) @type)
+
+(directive) @type
+
+; Properties
+;-----------
+
+(field
+  (name) @property)
+
+(field
+  (alias
+    (name) @property))
+
+(field_definition
+  (name) @property)
+
+(object_value
+  (object_field
+    (name) @property))
+
+; Variable Definitions and Arguments 
+;-----------------------------------
+
+(operation_definition
+  (name) @variable)
+
+(argument
+  (name) @parameter)
+
+(arguments_definition
+  (input_value_definition
+    (name) @parameter))
+
+(variable_definition
+  (variable) @parameter)
+
+(argument
+  (value
+    (variable) @variable))
+
+; Constants
+;----------
+
+(string_value) @string
+
+(int_value) @number
+
+(float_value) @float
+
+(boolean_value) @boolean
+
+; Literals
+;---------
+
 (description) @comment
 
-(name) @variable
+(directive_location
+  (executable_directive_location) @type.builtin)
 
-(fields_definition (field_definition (name) @keyword))
+(directive_location
+  (type_system_directive_location) @type.builtin)
 
-[
-  "QUERY"
-  "MUTATION"
-  "SUBSCRIPTION"
-  "FIELD"
-  "FRAGMENT_DEFINITION"
-  "FRAGMENT_SPREAD"
-  "INLINE_FRAGMENT"
-  "VARIABLE_DEFINITION"
-  "SCHEMA"
-  "SCALAR"
-  "OBJECT"
-  "FIELD_DEFINITION"
-  "ARGUMENT_DEFINITION"
-  "INTERFACE"
-  "UNION"
-  "ENUM"
-  "ENUM_VALUE"
-  "INPUT_OBJECT"
-  "INPUT_FIELD_DEFINITION"
-] @type.builtin
-
-; Tokens
-; ------
-
+; Keywords
+;----------
 
 [
- "|"
- "&"
-] @punctuation.delimiter
+  "query"
+  "mutation"
+  "subscription"
+  "fragment"
+  "scalar"
+  "type"
+  "interface"
+  "union"
+  "enum"
+  "input"
+  "extend"
+  "directive"
+  "schema"
+  "on"
+  "repeatable"
+] @keyword
 
-[
- "="
-] @operator
+; Punctuation
+;------------
 
 [
  "("
@@ -48,14 +137,11 @@
  "}"
 ] @punctuation.bracket
 
-[
-  "scalar"
-  "type"
-  "interface"
-  "union"
-  "enum"
-  "input"
-  "extend"
-  "directive"
-  "on"
-] @keyword
+"=" @operator
+
+"|" @punctuation.delimiter
+"&" @punctuation.delimiter
+":" @punctuation.delimiter
+
+"..." @punctuation.special
+"!" @punctuation.special
